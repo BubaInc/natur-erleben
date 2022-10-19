f5c9601985ec27a8c80def3581da0ac44cce8ca2
BubaInc <neumeier.nicolas@outlook.com>
2022-10-19 18:34:02 +0200

    new and improved

--- natur-erleben/pages/result.tsx
+++ natur-erleben/pages/result.tsx
@@ -3,19 +3,18 @@ import Container from "@mui/material/Container"
 import ListItemText from "@mui/material/ListItemText"
 import { useState } from "react"
 import { watchPlayerList } from "../util/Firebase"
-import { useItems } from "../util/StorageHandler"
 
 export default function Result() {
-  const items = useItems((items) => {
-    watchPlayerList(items.gameId, (snapshot) => {
-      if (snapshot.val() == null) return
-      setPlayerData(snapshot.val())
-    })
-  })
-  const [playerData, setPlayerData] = useState<any>([])
+  // const items = useItems((items) => {
+  //   watchPlayerList(items.gameId, (snapshot) => {
+  //     if (snapshot.val() == null) return
+  //     setPlayerData(snapshot.val())
+  //   })
+  // })
+  // const [playerData, setPlayerData] = useState<any>([])
   return (
     <Container maxWidth="sm">
-      <List>
+      {/* <List>
         {[...playerData]
           .sort((a: any, b: any) => b.numberCorrect - a.numberCorrect)
           .map((player: any, i: number) => (
@@ -23,7 +22,7 @@ export default function Result() {
               {player.name + " " + player.numberCorrect}
             </ListItemText>
           ))}
-      </List>
+      </List> */}
     </Container>
   )
 }
