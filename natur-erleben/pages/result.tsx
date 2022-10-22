e68b4c71e008f6f9021a851c58b2fe32d288470b
BubaInc <neumeier.nicolas@outlook.com>
2022-10-22 22:51:55 +0200

    result screen fixed

--- natur-erleben/pages/result.tsx
+++ natur-erleben/pages/result.tsx
@@ -1,27 +1,33 @@
 import List from "@mui/material/List"
 import Container from "@mui/material/Container"
 import ListItemText from "@mui/material/ListItemText"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { downloadGameData, GameData, Player } from "../util/Firebase"
+import { getItem } from "../util/StorageHandler"
+import Typography from "@mui/material/Typography"
 
 export default function Result() {
-  // const items = useItems((items) => {
-  //   watchPlayerList(items.gameId, (snapshot) => {
-  //     if (snapshot.val() == null) return
-  //     setPlayerData(snapshot.val())
-  //   })
-  // })
-  // const [playerData, setPlayerData] = useState<any>([])
+  const [gameData, setGameData] = useState(new GameData("", "", -1, {}))
+  useEffect(() => {
+    const cachedGameId = getItem("gameId")
+    if (!cachedGameId) return
+    downloadGameData(cachedGameId).then((data) => setGameData(data))
+  }, [])
   return (
     <Container maxWidth="sm">
-      {/* <List>
-        {[...playerData]
-          .sort((a: any, b: any) => b.numberCorrect - a.numberCorrect)
-          .map((player: any, i: number) => (
+      <Typography variant="h2" sx={{ mb: 2 }}>
+        Endergebnis:
+      </Typography>
+      <List>
+        {Object.keys(gameData.players)
+          .map((key) => gameData.players[key])
+          .sort((a: Player, b: Player) => b.numberCorrect - a.numberCorrect)
+          .map((player: Player, i: number) => (
             <ListItemText key={i}>
               {player.name + " " + player.numberCorrect}
             </ListItemText>
           ))}
-      </List> */}
+      </List>
     </Container>
   )
 }
